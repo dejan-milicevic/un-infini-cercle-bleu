d6904f7d2ef130cdb3ce19313b5b277f4f99a1a9
Dejan Milicevic <dejo1986@yahoo.com>
2015-06-16 13:09:47 +0200

    Modify body {padding-top} property

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
     
-    $(document.body).css('padding-top', $('.navbar-default').height());
-    $(window).resize(function(){
-        $(document.body).css('padding-top', $('.navbar-default').height());
-    });
-    $('.navbar-toggle').on('click', function() {
-        $(document.body).css('padding-top', $('.navbar-default').height() - $('.navbar-collapse').height()); 
-    });
-    
     $('div[data-type="parallax"]').each(function(){
         var $obj = $(this); 
         $(window).scroll(function() {
