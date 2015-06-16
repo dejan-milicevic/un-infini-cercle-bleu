396d3278a922586396310f965868ae87616a1a90
Dejan Milicevic <dejo1986@yahoo.com>
2015-06-16 11:31:16 +0200

    Parallax complete

--- _/components/js/script.js
+++ _/components/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    
     $(document.body).css('padding-top', $('.navbar-default').height());
     $(window).resize(function(){
         $(document.body).css('padding-top', $('.navbar-default').height());
@@ -6,4 +7,16 @@ $(document).ready(function(){
     $('.navbar-toggle').on('click', function() {
         $(document.body).css('padding-top', $('.navbar-default').height() - $('.navbar-collapse').height()); 
     });
+    
+    $('div[data-type="parallax"]').each(function(){
+        var $obj = $(this); 
+        $(window).scroll(function() {
+            var yPos = -($(window).scrollTop() / $obj.data('speed')); 
+            // Put together our final background position
+            var coords = '0 '+ yPos + 'px';
+            // Move the background
+            $obj.css('background-position', coords);
+        }); //end scroll
+    }); //end each  
+    
 });
